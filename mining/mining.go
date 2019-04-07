b31dd782ee0a32af0e4b023ed4cee06c88fa104f
clarenous <gaoceyu@gmail.com>
2019-04-08 02:28:10 +0800

    add mining

---
+++ mining/mining.go
@@ -0,0 +1,112 @@
+package mining
+
+import (
+	"github.com/clarenous/go-capsule/consensus"
+	"github.com/clarenous/go-capsule/errors"
+	"github.com/clarenous/go-capsule/protocol"
+	"github.com/clarenous/go-capsule/protocol/types"
+)
+
+// createCoinbaseTx returns a coinbase transaction paying an appropriate subsidy
+// based on the passed block height to the provided address.  When the address
+// is nil, the coinbase transaction will instead be redeemable by anyone.
+func createCoinbaseTx(amount uint64, blockHeight uint64) (tx *types.Tx, err error) {
+	amount += consensus.BlockSubsidy(blockHeight)
+	tx = types.MockTx()
+	txOut := types.MockTxOut()
+	txOut.Value = amount
+	tx.Inputs = []types.TxIn{}
+	tx.Outputs = []types.TxOut{*txOut}
+
+	return tx, nil
+}
+
+// NewBlockTemplate returns a new block template that is ready to be solved
+func NewBlockTemplate(c *protocol.Chain, txPool *protocol.TxPool) (b *types.Block, err error) {
+	//view := state.NewUtxoViewpoint()
+	//txEntries := []*bc.Tx{nil}
+	//txFee := uint64(0)
+	//
+	//// get preblock info for generate next block
+	b = types.MockBlock()
+
+	preBlockHeader := c.BestBlockHeader()
+	preBlockHash := preBlockHeader.Hash()
+	nextBlockHeight := preBlockHeader.Height + 1
+
+	b.Previous = preBlockHash
+	b.Height = nextBlockHeight
+	//nextBits, err := c.CalcNextBits(&preBlockHash)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//b = &types.Block{
+	//	BlockHeader: types.BlockHeader{
+	//		Version:           1,
+	//		Height:            nextBlockHeight,
+	//		PreviousBlockHash: preBlockHash,
+	//		Timestamp:         uint64(time.Now().Unix()),
+	//		BlockCommitment:   types.BlockCommitment{},
+	//		Bits:              nextBits,
+	//	},
+	//}
+	//bcBlock := &bc.Block{BlockHeader: &bc.BlockHeader{Height: nextBlockHeight}}
+	//b.Transactions = []*types.Tx{nil}
+	//
+	//txs := txPool.GetTransactions()
+	//sort.Sort(byTime(txs))
+	//for _, txDesc := range txs {
+	//	tx := txDesc.Tx.Tx
+	//	gasOnlyTx := false
+	//
+	//	if err := c.GetTransactionsUtxo(view, []*bc.Tx{tx}); err != nil {
+	//		blkGenSkipTxForErr(txPool, &tx.ID, err)
+	//		continue
+	//	}
+	//
+	//	gasStatus, err := validation.ValidateTx(tx, bcBlock)
+	//	if err != nil {
+	//		if !gasStatus.GasValid {
+	//			blkGenSkipTxForErr(txPool, &tx.ID, err)
+	//			continue
+	//		}
+	//		gasOnlyTx = true
+	//	}
+	//
+	//	if gasUsed+uint64(gasStatus.GasUsed) > consensus.MaxBlockGas {
+	//		break
+	//	}
+	//
+	//	if err := view.ApplyTransaction(bcBlock, tx, gasOnlyTx); err != nil {
+	//		blkGenSkipTxForErr(txPool, &tx.ID, err)
+	//		continue
+	//	}
+	//
+	//	if err := txStatus.SetStatus(len(b.Transactions), gasOnlyTx); err != nil {
+	//		return nil, err
+	//	}
+	//
+	//	b.Transactions = append(b.Transactions, txDesc.Tx)
+	//	txEntries = append(txEntries, tx)
+	//	gasUsed += uint64(gasStatus.GasUsed)
+	//	txFee += txDesc.Fee
+	//
+	//	if gasUsed == consensus.MaxBlockGas {
+	//		break
+	//	}
+	//}
+
+	// creater coinbase transaction
+	b.Transactions[0], err = createCoinbaseTx(0, nextBlockHeight)
+	if err != nil {
+		return nil, errors.Wrap(err, "fail on createCoinbaseTx")
+	}
+
+	return b, err
+}
+
+//func blkGenSkipTxForErr(txPool *protocol.TxPool, txHash *bc.Hash, err error) {
+//	log.WithField("error", err).Error("mining block generation: skip tx due to")
+//	txPool.RemoveTransaction(txHash)
+//}
---
+++ mining/miningpool/miningpool.go
@@ -0,0 +1,120 @@
+package miningpool
+
+import (
+	"errors"
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/clarenous/go-capsule/mining"
+	"github.com/clarenous/go-capsule/protocol"
+	"github.com/clarenous/go-capsule/protocol/types"
+)
+
+const (
+	maxSubmitChSize = 50
+)
+
+type submitBlockMsg struct {
+	blockHeader *types.BlockHeader
+	reply       chan error
+}
+
+// MiningPool is the support struct for p2p mine pool
+type MiningPool struct {
+	mutex    sync.RWMutex
+	block    *types.Block
+	submitCh chan *submitBlockMsg
+
+	chain      *protocol.Chain
+	txPool     *protocol.TxPool
+	newBlockCh chan *types.Hash
+}
+
+// NewMiningPool will create a new MiningPool
+func NewMiningPool(c *protocol.Chain, txPool *protocol.TxPool, newBlockCh chan *types.Hash) *MiningPool {
+	m := &MiningPool{
+		submitCh:   make(chan *submitBlockMsg, maxSubmitChSize),
+		chain:      c,
+		txPool:     txPool,
+		newBlockCh: newBlockCh,
+	}
+	m.generateBlock()
+	go m.blockUpdater()
+	return m
+}
+
+// blockUpdater is the goroutine for keep update mining block
+func (m *MiningPool) blockUpdater() {
+	for {
+		select {
+		case <-m.chain.BlockWaiter(m.chain.BestBlockHeight() + 1):
+			m.generateBlock()
+
+		case submitMsg := <-m.submitCh:
+			err := m.submitWork(submitMsg.blockHeader)
+			if err == nil {
+				m.generateBlock()
+			}
+			submitMsg.reply <- err
+		}
+	}
+}
+
+// generateBlock generates a block template to mine
+func (m *MiningPool) generateBlock() {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	block, err := mining.NewBlockTemplate(m.chain, m.txPool)
+	if err != nil {
+		log.Errorf("miningpool: failed on create NewBlockTemplate: %v", err)
+		return
+	}
+	m.block = block
+}
+
+// GetWork will return a block header for p2p mining
+func (m *MiningPool) GetWork() (*types.BlockHeader, error) {
+	if m.block != nil {
+		m.mutex.RLock()
+		defer m.mutex.RUnlock()
+		bh := m.block.BlockHeader
+		return &bh, nil
+	}
+	return nil, errors.New("no block is ready for mining")
+}
+
+// SubmitWork will try to submit the result to the blockchain
+func (m *MiningPool) SubmitWork(bh *types.BlockHeader) error {
+	reply := make(chan error, 1)
+	m.submitCh <- &submitBlockMsg{blockHeader: bh, reply: reply}
+	err := <-reply
+	if err != nil {
+		log.WithFields(log.Fields{"err": err, "height": bh.Height}).Warning("submitWork failed")
+	}
+	return err
+}
+
+func (m *MiningPool) submitWork(bh *types.BlockHeader) error {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	if m.block == nil || bh.Previous != m.block.Previous {
+		return errors.New("pending mining block has been changed")
+	}
+
+	m.block.Proof = bh.Proof
+	m.block.Timestamp = bh.Timestamp
+	isOrphan, err := m.chain.ProcessBlock(m.block)
+	if err != nil {
+		return err
+	}
+	if isOrphan {
+		return errors.New("submit result is orphan")
+	}
+
+	blockHash := bh.Hash()
+	m.newBlockCh <- &blockHash
+	return nil
+}
